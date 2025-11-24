dcbfd12fd26ecd8bb1fafd6744dcbe1d51a18205
Diego Guerrero <sdiegogro@hotmail.com>
2025-11-24 11:09:15 -0600

    Ajustes en el HTML, CORREGIR CUADRO DE SIGNUP

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
 console.log("Wishlink listo ✨");
+document.addEventListener("DOMContentLoaded", () => {
+    const scrollDown = document.getElementById("scrollDown");
+    const secondSection = document.getElementById("segunda-parte");
+
+    window.addEventListener("scroll", () => {
+        const secondSectionTop = secondSection.getBoundingClientRect().top;
+
+        // Si la segunda sección ya está cerca de mostrarse → ocultar flecha
+        if (secondSectionTop < window.innerHeight * 0.7) {
+            scrollDown.style.opacity = "0";
+            scrollDown.style.pointerEvents = "none";
+        } else {
+            scrollDown.style.opacity = "1";
+            scrollDown.style.pointerEvents = "auto";
+        }
+    });
+});
